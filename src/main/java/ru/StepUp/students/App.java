b3056f06244bf4fff7630c55fdba0f38681e8bc6
Sabina Sultanova <sultanovasabina89@gmail.com>
2025-07-20 11:38:17 +0300

    first comit

---
+++ src/main/java/ru/StepUp/students/App.java
@@ -0,0 +1,13 @@
+package ru.StepUp.students;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
